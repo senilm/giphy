1f525e07bf085d5e3f0e2f9a5ebaf7f52bfd759f
senilm <senilmendapara.it20@scet.ac.in>
2023-11-08 03:24:17 +0530

    Added Debouncing

--- app/components/Feed.jsx
+++ app/components/Feed.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { useContext, useEffect, useState } from "react";
 import Pagination from "./Pagination";
 import { DataContext } from "../context/DataProvider";
@@ -10,15 +10,15 @@ const Feed = () => {
   const [type, setType] = useState('trending');
   const [currentPage, setCurrentPage] = useState(1);
   const { searchTerm } = useContext(DataContext);
-  const [isLoading, SetIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   const limit = 8;
 
   const calculateOffset = (page) => (page - 1) * limit;
 
   const apiUrl = `https://api.giphy.com/v1/gifs/${type}?api_key=${apiKey}&q=${searchTerm}&limit=${limit}&offset=${calculateOffset(currentPage)}`;
 
+  let debounceTimeout;
 
-  // make the type to search when user types in search box
 
   const fetchGif = async () => {
     const newType = searchTerm ? 'search' : 'trending';
@@ -28,15 +28,29 @@ const Feed = () => {
     if (response.ok) {
       const data = await response.json();
       setGifData(data.data);
-      SetIsLoading(false)
+      setIsLoading(false);
     } else {
       throw new Error('Failed to fetch data');
     }
   };
 
   useEffect(() => {
-    fetchGif();
-  }, [searchTerm,type,apiUrl]);
+    // Clear previous debounce timeout
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+    // Set a new debounce timeout
+    debounceTimeout = setTimeout(() => {
+      fetchGif();
+    }, 500); 
+    
+    // Clean up the timeout when the component unmounts
+    return () => {
+      if (debounceTimeout) {
+        clearTimeout(debounceTimeout);
+      }
+    };
+  }, [searchTerm, type, apiUrl]);
 
   const handlePreviousClick = () => {
     if (currentPage > 1) {
@@ -45,7 +59,7 @@ const Feed = () => {
   };
 
   const handleButtonClick = (num) =>{
-    setCurrentPage(num)
+    setCurrentPage(num);
   }
 
   const handleNextClick = () => {
@@ -56,37 +70,40 @@ const Feed = () => {
     <>
       {isLoading ? (
         <div className="min-w-screen">
-        <Loader />
+          <Loader />
         </div>
       ) : (
         <>
-        <div className="grid grid-cols-4 gap-4 mt-4">
-          {gifData.map((gif) => (
-            <div className="flex flex-col relative" key={gif.id}>
-              <div className="w-full overflow-hidden shadow-md p-2 rounded-lg">
-                <iframe
-                  src={gif.embed_url}
-                  alt="Gif"
-                  className="w-full h-auto relative hover:scale-125 transition-all"
+          <div className="grid grid-cols-4 gap-4 mt-4">
+            {gifData.map((gif) => (
+              <div className="flex flex-col relative" key={gif.id}>
+                <div className="w-full overflow-hidden shadow-md p-2 rounded-lg">
+                  <iframe
+                    src={gif.embed_url}
+                    alt="Gif"
+                    className="w-full h-auto relative hover:scale-125 transition-all"
                   />
+                </div>
+                <div className="p-2">
+                  <p className="text-center text-gray-700 font-semibold">{gif.title}</p>
+                </div>
               </div>
-              <div className="p-2">
-                <p className="text-center text-gray-700 font-semibold">{gif.title}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <Pagination
-        onPreviousClick={handlePreviousClick}
-        onNextClick={handleNextClick}
-        currentPage={currentPage}
-        handleButtonClick={handleButtonClick}
-        />
+            ))}
+          </div>
+          <Pagination
+            onPreviousClick={handlePreviousClick}
+            onNextClick={handleNextClick}
+            currentPage={currentPage}
+            handleButtonClick={handleButtonClick}
+          />
         </>
       )}
-      
     </>
   );
-  
 };
+
 export default Feed;
+
+
+
+
